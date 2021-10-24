e9897dbe2107a47e25b8a8909f90fabdb67d9e33
quang <quangho@wee.vn>
2021-10-24 19:38:43 +0700

    Initial Commit

---
+++ ChartSwiftUI/ChartSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartSwiftUIApp.swift
+//  ChartSwiftUI
+//
+//  Created by quang on 24/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct ChartSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
